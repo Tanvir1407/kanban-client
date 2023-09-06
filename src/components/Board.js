f4cbae8f81f761eb499027a5e5d5f228057183f1
MD. Tanvir <93488008+Tanvir1407@users.noreply.github.com>
2023-09-07 02:04:10 +0600

    Update Board.js

--- src/components/Board.js
+++ src/components/Board.js
@@ -7,7 +7,7 @@ export default function Board() {
     const [Data, setData] = useState(null)
 
     useEffect(() => {
-        fetch("http://localhost:5500")
+        fetch("https://kanban-server-topaz.vercel.app")
             .then(res =>res.json())
             .then(data =>setData(data))
              
